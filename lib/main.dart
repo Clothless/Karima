25b25206f3dfd09d57b9005f72410c385b8cf663
Ibrahil-Chaibedraa <chaibedraa.work@gmail.com>
2023-02-25 15:16:14 -0600

    Project start

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Container(
+          color: Colors.black,
+          child: const Text("Karima"),
+        ),
+      ),
+      bottomNavigationBar: const BottomAppBar(),
+    );
+  }
+}
